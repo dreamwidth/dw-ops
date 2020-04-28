135d78ae326e72bc506de91e61852aac6d942b66
Mark Smith <mark@dreamwidth.org>
2020-04-28 06:16:43 +0000

    updates

--- buildkite/restart-webs.sh
+++ buildkite/restart-webs.sh
@@ -3,21 +3,18 @@
 export PATH="$PATH:/root/bin"
 
 for HOST in $(hosts-web | grep -v web01); do
+    echo "Restarting $host..."
     ssh root@$HOST "bin/bounce-apache >/dev/null &"
 
     sleep 15
 
-    restart_ok () {
-        echo "$HOST restarted OK!"
-        exit 0
-    }
-
-    curl -s $HOST/admin/healthy | grep -q 'status=ok' && restart_ok
-
-    echo "$HOST failed startup!"
-    echo
-    ssh root@$HOST "tail -100 /var/log/apache2/error.log"
-    echo
-
-    exit 1
+    if curl -s $HOST/admin/healthy | grep -q 'status=ok'; then
+        echo " * $HOST restarted!"
+    else
+        echo " * $HOST failed startup!"
+        echo
+        ssh root@$HOST "tail -100 /var/log/apache2/error.log"
+        echo
+        exit 1
+    fi
 done
